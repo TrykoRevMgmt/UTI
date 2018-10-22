5046eb2a526e0b9846feab3dc558cd8ae7a87021
Adam <adampoznanski@outlook.com>
2018-10-22 16:02:35 -0400

    adjusted test UI, added links to samplle utm's

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 //sample JSON obj
-
-var JSONObj ={
-  zillow:{'phone':'412-444-4444', 'email':'zillowemail@email.com'},
-  Adwords:{'phone':'412-444-5555', 'email':'awemail@email.com'},
-  facebook:{'phone':'412-444-6666', 'email':'fbemail@email.com'}
+var JSONObj = {
+  zillow: {
+    'phone': '412-444-4444',
+    'email': 'zillowemail@email.com'
+  },
+  adwords: {
+    'phone': '412-444-5555',
+    'email': 'awemail@email.com'
+  },
+  facebook: {
+    'phone': '412-444-6666',
+    'email': 'fbemail@email.com'
+  }
 };
 
 
-window.onload = function () {
-
+window.onload = function() {
   var url = window.location.search.substring(1);
   var email = JSONObj[url].email;
   var phone = JSONObj[url].phone;
-
   document.getElementById('email').innerHTML = email;
   document.getElementById('phone').innerHTML = phone;
 };
