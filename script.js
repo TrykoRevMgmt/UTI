eb6e53b56a65cba4d335ba8aaffeeea4a2ff72ec
Adam <adampoznanski@outlook.com>
2018-11-13 15:05:15 -0500

    changed utm format to GA format added buttons for conversion tracking

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*jshint esversion: 6 */
 var xhttp = new XMLHttpRequest();
 //Event Listners that run script when action is completed ??add for bucket adjustemtns?
 //document.getElementById("bDate").addEventListener("input", eLstnr);
@@ -11,21 +11,28 @@ var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
         var JSONObj = JSON.parse(xhttp.responseText);
-        console.log(JSONObj);
-				var leadSourceArr = Object.keys(JSONObj);
-				var leadSource = '';
-				for (var i = 0; i < leadSourceArr.length; i++) {
-					leadSource += '<option>'+leadSourceArr[i]+'</option>' ;
-				}
-				document.getElementById('leadSource').innerHTML = leadSource;
-				console.log(leadSource);
-
-				document.getElementById('leadSource').addEventListener("input", refreshUTM);
-        var url = window.location.search.substring(1);
-        var email = JSONObj[url].email;
-        var phone = JSONObj[url].phoneNum;
+				// var leadSourceArr = Object.keys(JSONObj);
+				// var leadSource = '';
+				// for (let i = 0; i < leadSourceArr.length; i++) {
+				// 	leadSource += '<option>'+leadSourceArr[i]+'</option>' ;
+				// }
+				// document.getElementById('leadSource').innerHTML = leadSource;
+				//
+				// document.getElementById('leadSource').addEventListener("input", refreshUTM);
+        var url = window.location.search.substring(1).split('&');
+				var utmObj = {};
+				for (let i = 0; i < url.length; i++) {
+						let Param = url[i].split('=');
+						let key = Param[0];
+						let value = Param[1];
+						utmObj[Param[0]] = Param[1];
+				};
+				console.log(window.location.href);
+        var email = JSONObj[utmObj.utm_source].email;
+        var phone = JSONObj[utmObj.utm_source].phone;
         document.getElementById('email').innerHTML = email;
         document.getElementById('phone').innerHTML = phone;
+				document.getElementById('utm').innerHTML = 'www.(insert page url here).com'+ window.location.search;
       }
     };
 
@@ -43,14 +50,3 @@ var testObj = {
 	phone: '421-444-4444',
 	email: 'adam@mail.com'
 };
-
-	const scriptURL = 'https://script.google.com/macros/s/AKfycbzazLgaOkVvFv8VehD-hdgOGUvwo-MIGbOzi2Q7SdmqhXT5prPr/exec'
-	const form = document.forms['contact'];
-	form.addEventListener('submit', e => {
-	e.preventDefault()
-	var data = new FormData(form);
-	var data2 = JSON.stringify(testObj);
-	fetch(scriptURL, { method: 'POST', body: data})
-	 .then(response => console.log('Success!', response))
-	 .catch(error => console.error('Error!', error.message))
-	});;
